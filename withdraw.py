a696fa6a39d9754e5bbcaf32e17805e8fe406c9c
Roman Arkhip <roman.itdtin@gmail.com>
2022-10-19 13:23:54 +0400

    fix

--- withdraw.py
+++ withdraw.py
@@ -4,26 +4,33 @@ import argparse
 
 def withdraw(address: str, wallet: str):
     return """
-        import {Contract, utils, Wallet} from "zksync-web3";
-        import * as ethers from "ethers";
-        import { HardhatRuntimeEnvironment } from "hardhat/types";
-        import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
-        
-        // The address of the counter smart contract
-          const ADDRESS = "%s";
-          // The ABI of the counter smart contract
-          const ABI = require("../abi/IO.json");
-        
-          const wallet = new Wallet("%s");
-        
-          // Create deployer object and load the artifact of the contract we want to deploy.
-          const deployer = new Deployer(hre, wallet);
-          const contract = new Contract(ADDRESS, ABI, deployer.zkWallet);
-        
-          console.log(`The contract is ${await contract.address}`);
-          await contract.withDrawAll()
-        }
+import {Contract, Wallet} from "zksync-web3";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+export default async function (hre: HardhatRuntimeEnvironment) {
+  // The address of the counter smart contract
+  const ADDRESS = "%s";
+  // The ABI of the counter smart contract
+  const ABI = require("../abi/IO.json");
+
+  const wallet = new Wallet("%s");
+
+  // Create deployer object and load the artifact of the contract we want to deploy.
+  const deployer = new Deployer(hre, wallet);
+  const contract = new Contract(ADDRESS, ABI, deployer.zkWallet);
+
+  console.log(`The contract is ${await contract.address}`);
+  await contract.withDrawAll()
+}
     """ % (address, wallet)
+
+
+def create_deploy(contractAddress: str, wallet: str) -> None:
+    if "deploy" not in os.listdir():
+        os.mkdir("deploy")
+    with open(f"deploy/deploy.ts", "w") as file:
+        file.writelines(withdraw(contractAddress, wallet))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,4 +38,4 @@ if __name__ == '__main__':
     parser.add_argument("-contractAddress", dest="contractAddress", required=True)
 
     args = parser.parse_args()
-    withdraw(args.contractAddress, args.wallet)
+    create_deploy(args.contractAddress, args.wallet)
